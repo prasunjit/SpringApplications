6cae1f3ce333939ad9cc7211d527d849180cd684
Prasunjit <prasunite@mail.com>
2017-02-18 12:39:47 -0600

    Added home.jsp and HomeController.java

---
+++ zcloth/src/main/java/com/store/zcloth/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.store.zcloth.controller;
+
+import javax.xml.ws.RequestWrapper;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home(){
+		return "home";
+		
+		
+	}
+	
+
+}
